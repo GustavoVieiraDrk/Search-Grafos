f232ebf00e540c56275fcbb08ed545f190b3de72
Gustavo_Vs <gustavovieira@vago.online>
2021-10-27 09:51:46 -0300

    Searching in grafos

---
+++ main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "Headers/busca_grafo.h"
+#include "Headers/fila_dinamica.h"
+
+int main(){
+    /*Grafo g1 = cria_grafo(false);
+
+    insere_vertice(g1, "A");
+    insere_vertice(g1, "B");
+    insere_vertice(g1, "C");
+    insere_vertice(g1, "D");
+    insere_vertice(g1, "E");
+    insere_vertice(g1, "F");
+    insere_vertice(g1, "G");
+    insere_vertice(g1, "H");
+    insere_vertice(g1, "I");
+    insere_vertice(g1, "J");
+
+    //print_matriz(g1);
+
+    insere_aresta(g1, "A", "B", 1);
+    insere_aresta(g1, "B", "D", 1);
+    insere_aresta(g1, "B", "E", 1);
+    insere_aresta(g1, "E", "H", 1);
+    insere_aresta(g1, "E", "I", 1);
+    insere_aresta(g1, "A", "C", 1);
+    insere_aresta(g1, "C", "F", 1);
+    insere_aresta(g1, "C", "G", 1);
+    insere_aresta(g1, "G", "J", 1);
+
+    //imprime_peso(g1, "joao", "maria");
+    //imprime_peso(g1, "jose", "mariana");
+
+    //print_matriz(g1);
+
+    /* imprime_grau(g1, "marina");
+    imprime_grau(g1, "jose");
+    imprime_grau(g1, "maria");
+
+    desenhe_grafo(g1);
+
+    if(sao_adjacentes(g1, "joao", "maria"))
+        printf("joao e maria sao adjancentes\n");
+
+    if(sao_adjacentes(g1, "tadeu", "mariana"))
+        printf("tadeu e mariana sao adjancentes\n");
+
+    imprime_adjacentes(g1, "joao");
+    imprime_adjacentes(g1, "jose");
+
+    busca_profundidade(g1, "A");
+    printf("\n");
+    busca_largura(g1, "A");
+    printf("\n");
+    busca_profundidade(g1, "E");
+    printf("\n");
+    busca_largura(g1, "E");
+
+    libera_grafo(g1);
+
+    printf("Ok\n");*/
+
+    escreve_tela();
+
+    return 0;
+}
